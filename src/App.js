60134175506309cbbf5dba42477b18289b762ad1
shell <shellipov@mail.ru>
2020-12-15 23:00:44 +0300

    maket

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PageOne from "./components/PageOne";
+import PageTwo from "./components/PageTwo";
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/page2" component={PageTwo} />
+          <Route exact path="/" component={PageOne} />
+        </Switch>
+      </Router>
+    </Provider>
+    </>
   );
 }
 
